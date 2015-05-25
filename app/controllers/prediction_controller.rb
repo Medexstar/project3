49ab650531a7a9cce87836dab3fc2df369c2bf5c
Derek Mui <derek678@hotmail.com>
2015-05-25 18:31:27 +1000

    Tidied up code

--- app/controllers/prediction_controller.rb
+++ app/controllers/prediction_controller.rb
@@ -1,8 +1,7 @@
 class PredictionController < ApplicationController
 	
 	def show_lat_long
-		@location = Location.get_nearest_location(params[:lat], params[:long])[0]
-		@hash = Prediction.predict(@location, params[:period], params[:lat], params[:long])
+		@hash = Prediction.predict_latlong(params[:period], params[:lat], params[:long])
 	    respond_to do |format|
 	      format.html
 	      format.json { render json: JSON.pretty_generate(@hash) }
@@ -10,7 +9,6 @@ class PredictionController < ApplicationController
 	end
 	
 	def show_postcode
-		@location = Location.get_nearest_location(params[:postcode])
 		@hash = Prediction.predict_postcode(params[:postcode], params[:period])
 		respond_to do |format|
 	      format.html
--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,35 +1,17 @@
 class Prediction < ActiveRecord::Base
-	def self.predict location, period, lat, long
+	def self.predict_latlong period, lat, long
+		location = Location.get_nearest_location(lat, long)[0]
 		if !location.last_update
 			return {}
 		end
-		current_time = Time.now
-		time_difference = current_time - location.measurements.first.timestamp
-		hash = {:lattitude => lat, :longitude => long, :predictions => {}}
-		data = location.measurements.all
-		rainfall_array, temp_array, winddir_array, windspeed_array, time_array = [], [] ,[] ,[] ,[]
+		data_hash = {}
+		result_hash = {:lattitude => lat, :longitude => long, :predictions => {}}
+		first_measurement = location.measurements.first.timestamp
 		
-		data.each do |data_point|
-			rainfall_array << data_point.precip_intensity
-			temp_array << data_point.temp
-			winddir_array << data_point.wind_direction
-			windspeed_array << data_point.wind_speed
-			time_array << (data_point.timestamp - location.measurements.first.timestamp)/60
-		end
+		get_data(location.measurements.all, data_hash, first_measurement)
 		
-		names = [:rain, :temp, :wind_direction, :wind_speed]
-		
-		(0..period.to_i/10).each do |time|
-			count = 0
-			hash[:predictions][(time*10).to_s] = {:time => (current_time + (time*10*60)).strftime("%-l:%M%P %d-%m-%Y")}
-			[rainfall_array, temp_array, winddir_array, windspeed_array].each do |y_array|
-				reg = Regression.calc_best_regression(time_array, y_array)
-				value = reg.calc_prediction(time_difference/60 + time*10).round(2)
-				hash[:predictions][(time*10).to_s][names[count]] = {:value => value.to_s, :probability => reg.r_sqrd.to_s}
-				count += 1
-			end
-		end
-		return hash
+		make_prediction(data_hash, period, result_hash, first_measurement)
+		return result_hash
 	end
 	
 	def self.predict_postcode postcode, period
@@ -40,7 +22,6 @@ class Prediction < ActiveRecord::Base
 		end
 		data_hash = {}
 		result_hash = {:postcode => postcode, :predictions => {}}
-		rainfall_array, temp_array, winddir_array, windspeed_array, time_array = [], [] ,[] ,[] ,[]
 		first_measurement = Time.now
 		locations.each do |location|
 			if location.measurements.first.timestamp < first_measurement
@@ -50,15 +31,8 @@ class Prediction < ActiveRecord::Base
 		locations.each do |location|
 			get_data(location.measurements.all, data_hash, first_measurement)
 		end
-		data_hash.each do |time, measurements|
-			time_array << time
-			rainfall_array << measurements[:rainfall]
-			temp_array << measurements[:temp]
-			winddir_array << measurements[:winddir]
-			windspeed_array << measurements[:windspeed]
-		end
 		
-		make_prediction(rainfall_array, temp_array, winddir_array, windspeed_array, time_array, period, result_hash, first_measurement)
+		make_prediction(data_hash, period, result_hash, first_measurement)
 		return result_hash
 	end
 	
@@ -69,10 +43,19 @@ class Prediction < ActiveRecord::Base
 		end
 	end
 	
-	def self.make_prediction rainfall_array, temp_array, winddir_array, windspeed_array, time_array, period, hash, first_measurement
+	def self.make_prediction data_hash, period, hash, first_measurement
 		current_time = Time.now
 		time_difference = current_time - first_measurement
 		names = [:rain, :temp, :wind_direction, :wind_speed]
+		rainfall_array, temp_array, winddir_array, windspeed_array, time_array = [], [] ,[] ,[] ,[]
+		
+		data_hash.each do |time, measurements|
+			time_array << time
+			rainfall_array << measurements[:rainfall]
+			temp_array << measurements[:temp]
+			winddir_array << measurements[:winddir]
+			windspeed_array << measurements[:windspeed]
+		end
 		
 		(0..period.to_i/10).each do |time|
 			count = 0
